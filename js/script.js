d195fdd2c7e5c7cbd4442bdea5feb29799badea7
Eugeniy <39237107+drobov1k@users.noreply.github.com>
2020-04-30 10:17:49 +0300

    first commit -gruzoperevozki

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const SCROLL_HIDDEN_NAV_VALUE = 150;
+const REQUEST_URL_ADDRESS = '';
+
+let $el     = document.querySelector('.nav');
+let $burger = document.querySelector('.nav__burger');
+let $menu   = document.querySelector('.nav__menu');
+
+window.onscroll = () => {
+    let has = $el.classList.contains('opacity');
+    window.pageYOffset > SCROLL_HIDDEN_NAV_VALUE ?
+    has ? `` : $el.classList.add('opacity') :
+    has ? $el.classList.remove('opacity') : ``
+};
+
+$burger.addEventListener('click', () => {
+    $burger.classList.toggle('active');
+    $menu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav__item-href')
+    .forEach(item => {
+        item.addEventListener('click', e => {
+            $burger.classList.remove('active');
+            $menu.classList.remove('active');
+            const hash = item.href.split('#')[1];
+            if (hash === 'feedback' && document.getElementById(hash).hidden)
+                document.getElementById(hash).hidden = false;
+        })
+    });
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // код для мобильных устройств
+} else {
+    // код для обычных устройств
+}
